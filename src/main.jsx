d3e68685c06954e4342006f2803514d0eec63d68
Ayub40 <ayubk4028@gmail.com>
2024-04-08 22:23:45 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './components/Routes.jsx'
+import AuthProviders from './providers/AuthProviders.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProviders>
+      {/* <RouterProvider router={router} /> */}
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProviders>
+  </React.StrictMode>,
+)
